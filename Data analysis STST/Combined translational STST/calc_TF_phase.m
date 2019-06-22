4efb14394936a1507578b6c8d06eea312b62bf19
George Dialynas <georgedialynas@hotmail.gr>
2019-06-22 15:38:26 +0200

    Delete calc_TF_phase.m

--- Data analysis STST/Combined translational STST/calc_TF_phase.m
+++
@@ -1,9 +0,0 @@
-function [wtf_phase_SP_Z] = calc_TF_phase(wtf)
-
-[~,col]         = size(wtf);
-wtf_phase_SP_Z  = NaN(size(wtf));
-
-for i = 1:col
-    wtf_phase_SP_Z_sub = angle(wtf(:,i))/(pi/180);
-    wtf_phase_SP_Z(:,i) = wtf_phase_SP_Z_sub;
-end
