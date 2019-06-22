20e113c1363ddb054ddb244180e25122a9eb77d4
George Dialynas <georgedialynas@hotmail.gr>
2019-06-22 15:38:17 +0200

    Delete calc_TF_magnitude.m

--- Data analysis STST/Combined translational STST/calc_TF_magnitude.m
+++
@@ -1,9 +0,0 @@
-function [wtf_mag] = calc_TF_magnitude(wtf)
-
-[~,col] = size(wtf);
-wtf_mag = NaN(size(wtf));
-
-for i = 1:col
-    wtf_mag_sub  = abs(wtf(:,i));
-    wtf_mag(:,i) = wtf_mag_sub;
-end
